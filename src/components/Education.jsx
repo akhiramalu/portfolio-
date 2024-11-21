b751c422a85c2b39d6169fa7ad830effc2b983b3
Vipin B <vipin.b@seqato.com>
2024-11-21 23:07:03 +0530

    changed education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,10 +3,37 @@ import img1 from '../components/IMAGE/malu1.jpg'
 
 const Education = () => {
   return (
-    <div></div>
+    <div class="container-fluid">
+    <div class="header">
+     <div class="wrapper">
+  <div class="bg"> education </div>
+  <div class="fg"> education </div>
+</div>
 
 
-          
+
+    </div>
+    <div class="row">
+    <div class="body2">
+    <div class = "container2">
+    <div class = "card">
+      <div class = "image">
+      <img src={img1} />
+           
+      </div>
+      <div class = "content">
+        <h3>This is content</h3>
+        <p>DIn publishing and graphic design,           Lorem ipsum is a placeholder text               commonly used to demonstrate the visual         form of a document or a typeface without         relying on meaningful content.</p>
+      </div>
+     
+  </div>
+    </div>    
+   </div> 
+  
+  </div>
+    
+  </div>
+ 
             
   )
 }
