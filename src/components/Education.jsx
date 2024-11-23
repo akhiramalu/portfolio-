5000a5396d34774f141cbe8e63e15d9a60567a32
Vipin B <vipin.b@seqato.com>
2024-11-23 17:59:36 +0530

    changed education card

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import img1 from '../components/IMAGE/malu1.jpg'
+import hschool from '../components/IMAGE/ischool.jpg'
+import ishool from '../components/IMAGE/highschool.avif'
+
+import degree from '../components/IMAGE/degree.png'
+import net from '../components/IMAGE/net.jpg'
+
 
 const Education = () => {
   return (
@@ -11,16 +16,44 @@ const Education = () => {
 </div>
 
 
-<div class="ele">
-   
-    <div class="box">hello</div>
-    <div class="box">More Scroll Animations</div>
-    <div class="box">Keep Scrolling!</div>
+
+ <div class="body5">
+  <div class="row">
+    <div class="card1">
+    <img src={hschool}  class="hi"/>
+    <div class="card-img-overlay">
+      <h1 class="h1"> MY <span>SCHOOLING</span></h1>
+      <p class="p">G.V.H.S.S PIRAPPANCODE</p>
+    </div>
+    </div>
+    <div class="card1">
+    <img src={ishool}  class="hi"/>
+    <div class="card-img-overlay">
+      <h1 class="h2">HIGH SCHOOL</h1>
+      <p class="h2">G H S S PIRAPPANCODE</p>
+    </div>
+    </div>
+    <div class="card1">
+    <img src={degree}  class="hi"/>
+    <div class="card-img-overlay">
+      <h1 class="h3"> BACHELOR'S DEGREE </h1>
+      <p>UNIVERSTY INSTITUTE OF TECHNOLOGY
+        PIRAPPANCODE
+      </p>
+    </div>
+    </div>
+    <div class="card1">
+    <img src={net}  class="hi"/>
+    <div class="card-img-overlay">
+      <h1 class="h4">PYTHON FULLSTACK</h1>
+      {/* <p>NETWOK SYSTEMS</p> */}
+      </div>
+    </div>
   </div>
-    
-    
   </div>
- </div>         
+  </div>
+  </div>
+
   )
 }
 
